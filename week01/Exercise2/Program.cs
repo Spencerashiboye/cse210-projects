c189ec4fe21bc84ecf1e08234b7f85d7c2d3ccea
Spencerashiboye <ashiboyespencer19@gmail.com>
2025-01-13 10:36:22 +0000

    new

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade precentage? ");
+        string grade = Console.ReadLine();
+        int precent = int.Parse(grade);
+
+        string letter = "";
+
+        if (precent >= 90)
+        {
+            letter = "A";
+        }
+        else if (precent >= 80)
+        {
+            letter = "B";
+        }
+        else if (precent >= 70)
+        {
+            letter = "C";
+        }
+        else if (precent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (precent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+    
     }
 }
